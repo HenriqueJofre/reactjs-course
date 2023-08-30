3766031db88da0a5da77a35bbb116328d3b338d7
Henrique Jofre <hss.jofre@gmail.com>
2023-08-30 17:28:07 -0300

    Understanding JSX and its relationship to Javascript

--- 01-jsx-components/src/App.js
+++ 01-jsx-components/src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const element = <h1>Hello, my name is Henrique Jofre</h1>;
+
+  const name = "Henrique Jofre";
+  const setName = <h1>What is your name? My name is {name}</h1>
+
+  const world = React.createElement("h1", null, "Hello,", " ", "World!", " Let's, go to learn ReactJS");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p>
+            {world}
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+
+      <div>
+        <h1>This is a second element</h1>
+      </div>
+    </>
   );
 }
 
